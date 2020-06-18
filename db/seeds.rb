de68d487019775b7055095411b65ddd26a4f8c0d
SatnamSingh <satnam.ss940@gmail.com>
2020-06-18 17:10:52 +0530

    login and chatscreen page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Satnam Singh", password: "password")
+User.create(username: "Satnam", password: "password")
+User.create(username: "Test", password: "password")
+User.create(username: "User1", password: "password")
+User.create(username: "Tom", password: "password")
